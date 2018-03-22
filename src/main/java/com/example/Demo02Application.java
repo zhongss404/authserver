916a120c8b1bd1c7403256be2369cd0b2e1a9f0e
zhongshaoshuai <437998278@qq.com>
2018-03-22 12:56:54 +0800

    将项目打包方式由jar包变为war

--- src/main/java/com/example/Demo02Application.java
+++ src/main/java/com/example/Demo02Application.java
@@ -3,11 +3,17 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.example.dao.mapper")
-public class Demo02Application {
+public class Demo02Application extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Demo02Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Demo02Application.class, args);
 	}
